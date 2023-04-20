11f6ce0d3e43ae57eb68bed64c4e9ec5916950fa
tejas patel <tejas_patel@colpal.com>
2023-04-20 14:55:31 -0400

    adding q tag addition

--- macros/query_tag.sql
+++ macros/query_tag.sql
@@ -1,10 +1,14 @@
-{% macro set_query_tag() -%}
-  {% set new_query_tag = model.name %} 
-  {% if new_query_tag %}
-    {% set original_query_tag = get_current_query_tag() %}
-    {{ log("Setting query_tag to '" ~ new_query_tag ~ "'. Will reset to '" ~ original_query_tag ~ "' after materialization.") }}
+-- {% macro set_query_tag() -%}
+--   {% set new_query_tag = model.name %} 
+--   {% if new_query_tag %}
+--     {% set original_query_tag = get_current_query_tag() %}
+--     {{ log("Setting query_tag to '" ~ new_query_tag ~ "'. Will reset to '" ~ original_query_tag ~ "' after materialization.") }}
+--     {% do run_query("alter session set query_tag = '{}'".format()) %}
+--     {{ return(original_query_tag)}}
+--   {% endif %}
+--   {{ return(none)}}
+-- {%- endmacro %}
+{%- macro set_snowflake_query_tag(tag = '') %}
+    {% set new_query_tag = model.name %}
     {% do run_query("alter session set query_tag = '{}'".format({'model_name': new_query_tag, 'context_id': {{invocation_id}} })) %}
-    {{ return(original_query_tag)}}
-  {% endif %}
-  {{ return(none)}}
 {%- endmacro %}
