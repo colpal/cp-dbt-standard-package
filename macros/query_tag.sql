0a7c0c425555e0baccf7df7506eff2b7cd9092fe
tejas patel <tejas_patel@colpal.com>
2023-04-20 15:19:31 -0400

    ed

--- macros/query_tag.sql
+++ macros/query_tag.sql
@@ -10,5 +10,5 @@
 -- {%- endmacro %}
 {% macro set_query_tag() -%}
     {% set new_query_tag = model.name %}
-    {% do run_query("alter session set query_tag = {}".format(new_query_tag))) %}
+    {% do run_query("alter session set query_tag = {}".format(new_query_tag)) %}
 {% endmacro %}
