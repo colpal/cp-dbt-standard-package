aa32f2e8bc400d5cf0f570008e07e01c49803779
tejas patel <tejas_patel@colpal.com>
2023-04-24 09:15:39 -0400

    testing qtag sql

--- macros/set_query_tag.sql
+++ macros/set_query_tag.sql
@@ -1,3 +1,10 @@
-{% macro set_query_tag() %}
-  {% do run_query("alter session set query_tag = '{}'".format(model.name)) %}
+{% macro set_query_tag() -%}
+  {% set new_query_tag = model.name %}  
+
+  {% if new_query_tag %}
+    {% do run_query("alter session set query_tag = '{}'".format(model.name)) %}  
+  {% endif %}
+
+  {{ return(none)}}
+
 {% endmacro %}
