b23652ee4c03c19b4494e5fe0ae60c38c1e8eff4
tejas patel <tejas_patel@colpal.com>
2023-04-24 15:00:03 -0400

    edit macro

--- macros/set_query_tag.sql
+++ macros/set_query_tag.sql
@@ -1,5 +1,5 @@
 {%- macro set_snowflake_query_tag(tag = '') %}
-    {% set new_tag = '{{ invocation_id }}' + '-' + model.name %}
+    {% set new_tag = model.name %}
     {% do run_query("alter session set query_tag = '{}'".format(new_tag)) %}
 {%- endmacro %}
 
