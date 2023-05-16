d6261fe64102fc78b99f3754b5698ee52bff1b64
tejas patel <tejas_patel@colpal.com>
2023-05-16 12:55:25 -0400

    set qtag

--- macros/set_snowflake_query_tag.sql
+++ macros/set_snowflake_query_tag.sql
@@ -1,5 +1,5 @@
 {%- macro set_snowflake_query_tag(tag = '') %}
-    {% set new_tag = model.name %}
-    {% do run_query('alter session set query_tag = "{}"'.format(dict(invocation_id = invocation_id, model_name = new_tag))) %}
+    {% set model = model.name %}
+    {% do run_query('alter session set query_tag = "{}"'.format(dict(invocation_id = invocation_id, model_name = model))) %}
 {%- endmacro %}
 
