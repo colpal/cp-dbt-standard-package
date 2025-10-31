e6538f676e56ca4e41c21feb7102c96f4a8a9da1
bp-jeremy-nguyen-cp <bp_jeremy_nguyen@colpal.com>
2025-10-30 22:13:22 -0400

    Update apply_snowflake_tags.sql

--- macros/apply_snowflake_tags.sql
+++ macros/apply_snowflake_tags.sql
@@ -121,7 +121,7 @@
     {{ log("Applied tag '" ~ tag_name ~ "' to " ~ schema ~ "." ~ identifier, info=true) }}
 {% endmacro %}
 
-{% macro apply_column_tag(schema, identifier, column_name, tag_name, tag_value, relation_type=none) %}
+{% macro apply_column_tag(database_nm, schema, identifier, column_name, tag_name, tag_value, relation_type=none) %}
     {# get available tags for validation #}
     {% set config = cp_dbt_standard_package.get_tag_config() %}
     {% set available_tags = cp_dbt_standard_package.get_snowflake_tags() %}
@@ -229,7 +229,7 @@
                     {% if column.meta is defined and column.meta.snowflake_tags is defined %}
                         {{ log("Processing column: " ~ column_name, info=true) }}
                         {% for tag_name, tag_value in column.meta.snowflake_tags.items() %}
-                            {{ cp_dbt_standard_package.apply_column_tag(model_schema, model_name, column_name, tag_name, tag_value, 'TABLE') }}
+                            {{ cp_dbt_standard_package.apply_column_tag(model_database, model_schema, model_name, column_name, tag_name, tag_value, 'TABLE') }}
                         {% endfor %}
                     {% endif %}
                 {% endfor %}
