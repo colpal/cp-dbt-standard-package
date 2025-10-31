f80c29c3af0fe3d37b4294c72e0a1d41e33be35b
bp-jeremy-nguyen-cp <bp_jeremy_nguyen@colpal.com>
2025-10-30 20:23:39 -0400

    Update apply_snowflake_tags.sql

--- macros/apply_snowflake_tags.sql
+++ macros/apply_snowflake_tags.sql
@@ -13,7 +13,7 @@
 
 -- retrieve all available Snowflake tags from central schema
 {% macro get_snowflake_tags() %}
-    {% set config = get_tag_config() %}
+    {% set config = cp_dbt_standard_package.get_tag_config() %}
     
     {% set sql %}
     SHOW TAGS IN SCHEMA {{ config.tag_database }}.{{ config.tag_schema }}
@@ -53,7 +53,7 @@
 -- apply tag to a model with validation
 {% macro apply_tag(database_nm, schema, identifier, tag_name, tag_value, relation_type=none) %}
     {# get available tags for validation #}
-    {% set config = get_tag_config() %}
+    {% set config = cp_dbt_standard_package.get_tag_config() %}
     {% set available_tags = get_snowflake_tags() %}
     
     {# use namespace for variables that need to persist outside the loop #}
@@ -118,7 +118,7 @@
 
 {% macro apply_column_tag(database_nm, schema, identifier, column_name, tag_name, tag_value, relation_type=none) %}
     {# get available tags for validation #}
-    {% set config = get_tag_config() %}
+    {% set config = cp_dbt_standard_package.get_tag_config() %}
     {% set available_tags = get_snowflake_tags() %}
     
     {# use namespace for variables that need to persist outside the loop #}
