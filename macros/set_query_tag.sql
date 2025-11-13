f056e4b073ec224eb9d52008900d171482985d7f
bp-jeremy-nguyen-cp <bp_jeremy_nguyen@colpal.com>
2025-11-13 02:31:39 -0500

    Update set_query_tag.sql

--- macros/set_query_tag.sql
+++ macros/set_query_tag.sql
@@ -2,9 +2,6 @@
     {% if not model is defined %}
         {% do return(None) %}
     {% endif %}
-    {% set model_name = model.name %}
-    {% set model_schema = model.schema if model.schema is not none else target.schema %}
-    {% set model_database = model.database if model.database is not none else target.database %}
     
     {% set relation = api.Relation.create(
         database='OPS_CUR', 
@@ -26,14 +23,11 @@
     {% set merged_extra = extra.copy() %}
     {% do merged_extra.update({
         'invocation_id': invocation_id, 
-        'model': model_name, 
-        'schema': model_schema,
-        'database': model_database,
+        'model': model.name, 
         'is_airflow_run': airflow_run
     }) %}
     
     {% set result = adapter.dispatch('set_query_tag', 'dbt_snowflake_query_tags')(extra=merged_extra) %}
     {% do run_query('USE WAREHOUSE "' ~ warehouseName.upper() ~ '"') %}
-    {{ return(result) }}
 
 {%- endmacro %}
