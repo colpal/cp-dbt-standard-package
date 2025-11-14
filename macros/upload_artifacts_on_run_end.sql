f13797244d0b957bc2824ff6dafdef740cdc30fc
bp-jeremy-nguyen-cp <bp_jeremy_nguyen@colpal.com>
2025-11-13 23:33:32 -0500

    Update upload_artifacts_on_run_end.sql

--- macros/upload_artifacts_on_run_end.sql
+++ macros/upload_artifacts_on_run_end.sql
@@ -3,7 +3,7 @@
     {% set artifacts_schema = env_var('DBT_ARTIFACTS_SCHEMA', '') %}
 
     {% if not artifacts_db or not artifacts_schema %}
-        {% do return(None) %}
+        {% do return('') %}
     {% endif %}
 
     {{ log("Uploading dbt artifacts to " ~ artifacts_db ~ "." ~ artifacts_schema, info=true) }}
@@ -13,5 +13,5 @@
 
     {{ dbt_artifacts.upload_results(results) }}
 
-    {% do return(None) %}
+    {% do return('') %}
 {% endmacro %}
