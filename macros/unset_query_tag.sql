f8483b46c2ba3817bda098060493352a231a12df
Sanil Varghese <67750482+SanilVarghese@users.noreply.github.com>
2026-03-16 16:37:14 +0530

    Update unset_query_tag.sql

--- macros/unset_query_tag.sql
+++ macros/unset_query_tag.sql
@@ -1,22 +1,9 @@
 {% macro unset_query_tag(original_query_tag) -%}
-
-    {# Check if dbt_snowflake_query_tags is installed #}
-    {% if dbt_snowflake_query_tags in context %}
-
-        {% do return(dbt_snowflake_query_tags.unset_query_tag(original_query_tag)) %}
-
-    {% else %}
-
-        {# Fallback logic if package is not installed #}
-        {% if original_query_tag %}
-            {% do run_query("alter session set query_tag = '{}'".format(original_query_tag)) %}
-        {% else %}
-            {% do run_query("alter session unset query_tag") %}
-        {% endif %}
-
+    {% if not model is defined %}
+        {% do return(None) %}
     {% endif %}
-
     {% set warehouseName = env_var('DBT_SF_WAREHOUSE') if warehouse else 'CP_DBT_XSMALL_WH_V2' %}
     {% do run_query('USE WAREHOUSE "' ~ warehouseName.upper() ~ '"') %}
+    {% do return(dbt_query_tags.unset_query_tag(original_query_tag)) %}
 
 {% endmacro %}
