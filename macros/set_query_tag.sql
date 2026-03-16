c09b714d151a58182b17f9ffb0ac868ed3ea967b
Sanil Varghese <67750482+SanilVarghese@users.noreply.github.com>
2026-03-16 16:36:40 +0530

    Update set_query_tag.sql

--- macros/set_query_tag.sql
+++ macros/set_query_tag.sql
@@ -1,43 +1,30 @@
 {% macro set_query_tag(extra = {}) -%}
-    
-    {# Get warehouse recommendation from lookup table #}
+
     {% set relation = api.Relation.create(
-        database='OPS_CUR',
-        schema='WH_RECOMMENDATIONS',
+        database='OPS_CUR', 
+        schema='WH_RECOMMENDATIONS', 
         identifier='DIM_WAREHOUSE_RECOMMENDATION'
     ) %}
     
-    {# Build query parameters #}
     {% set airflow_run = env_var('AIRFLOW_RUN', 'true') %}
-    {% set where_statement = "source_uri = '" ~ model.name ~ "' and airflow = '" ~ airflow_run ~ "'" %}
-    
-    {# Get recommended warehouse name #}
+    {% set where_statement = "source_uri = '" ~ model.name ~ "' and airflow = '" ~ airflow_run ~ "'" %}  
+
     {% set warehouse = dbt_utils.get_column_values(
-        relation,
-        'RECOMMENDED_WAREHOUSE_NAME',
-        default=['CP_DBT_LARGE_WH_V2'],
+        relation, 
+        'RECOMMENDED_WAREHOUSE_NAME', 
+        default=['CP_DBT_LARGE_WH_V2'], 
         where=where_statement
     ) %}
-    {% set warehouse_name = warehouse[0] if warehouse else 'CP_DBT_LARGE_WH_V2' %}
+    {% set warehouseName = warehouse[0] if warehouse else 'CP_DBT_LARGE_WH_V2' %}
     
-    {# Prepare query tag with model metadata #}
     {% set merged_extra = extra.copy() %}
     {% do merged_extra.update({
-        'invocation_id': invocation_id,
-        'model': model.name,
+        'invocation_id': invocation_id, 
+        'model': model.name, 
         'is_airflow_run': airflow_run
     }) %}
     
-    {# Set query tag using available method #}
-    {% if dbt_snowflake_query_tags in context %}
-        {# Use dbt_snowflake_query_tags package if available #}
-        {% set result = adapter.dispatch('set_query_tag', 'dbt_snowflake_query_tags')(extra=merged_extra) %}
-    {% else %}
-        {# Fallback to direct SQL command #}
-        {% do run_query('ALTER SESSION SET query_tag = "{}"'.format(merged_extra)) %}
-    {% endif %}
-    
-    {# Set warehouse for this session #}
-    {% do run_query('USE WAREHOUSE "' ~ warehouse_name.upper() ~ '"') %}
+    {% set result = adapter.dispatch('set_query_tag', 'dbt_query_tags')(extra=merged_extra) %}
+    {% do run_query('USE WAREHOUSE "' ~ warehouseName.upper() ~ '"') %}
 
 {%- endmacro %}
