c9e759c4ab8dcdd8770c28f17cefbef9ca59bfe3
Sanil Varghese <67750482+SanilVarghese@users.noreply.github.com>
2026-03-17 12:28:47 +0530

    Update set_query_tag.sql

--- macros/set_query_tag.sql
+++ macros/set_query_tag.sql
@@ -1,5 +1,5 @@
 {% macro set_query_tag(extra = {}) -%}
-
+    {% do run_query('use warehouse CP_DBT_XSMALL_WH_V2') %} # enabling recommendation query to run on xsmall warehouse 
     {% set relation = api.Relation.create(
         database='OPS_CUR', 
         schema='WH_RECOMMENDATIONS', 
