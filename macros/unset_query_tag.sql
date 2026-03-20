22a2c55849ee0005147950dc351df1b6edaf8e5e
Sanil Varghese <67750482+SanilVarghese@users.noreply.github.com>
2026-03-21 00:59:23 +0530

    Update unset_query_tag.sql

--- macros/unset_query_tag.sql
+++ macros/unset_query_tag.sql
@@ -2,7 +2,7 @@
     {% if not model is defined %}
         {% do return(None) %}
     {% endif %}
-    {% set warehouseName = env_var('DBT_SF_WAREHOUSE') if warehouse else 'CP_DBT_XSMALL_WH_V2' %}
+    {% set warehouseName = env_var('DBT_SF_WAREHOUSE') if warehouse else 'CP_DBT_XSMALL_WH' %}
     {% do run_query('USE WAREHOUSE "' ~ warehouseName.upper() ~ '"') %}
     {% do return(dbt_query_tags.unset_query_tag(original_query_tag)) %}
 
