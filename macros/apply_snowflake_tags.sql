5f5dadf259fe0462d8e1dc31042b0abc581e10da
bp-jeremy-nguyen-cp <bp_jeremy_nguyen@colpal.com>
2026-05-26 01:25:19 -0400

    update on run end macro

--- macros/apply_snowflake_tags.sql
+++ macros/apply_snowflake_tags.sql
@@ -314,7 +314,12 @@
    ============================================================ #}
 {% macro call_cross_domain_read_proc() %}
     {% if execute %}
-        {{ log("Calling GRANT_CROSS_DOMAIN_READ_ACCESS procedure to apply certified read grants", info=true) }}
-        {% do run_query("CALL OPS_CUR.UTIL_COMMON.GRANT_CROSS_DOMAIN_READ_ACCESS()") %}
+        {% set env_role = env_var('ROLE', '') || upper %}
+        {% set is_deploy_con_role = 'DEPLOY_CON' in env_role %}
+
+        {% if flags.WHICH == "build" and is_deploy_con_role %}
+            {{ log("Calling GRANT_CROSS_DOMAIN_READ_ACCESS procedure to apply crpss domain read grants", info=true) }}
+            {% do run_query("CALL OPS_CUR.UTIL_COMMON.GRANT_CROSS_DOMAIN_READ_ACCESS()") %}
+        {% endif %}
     {% endif %}
 {% endmacro %}
