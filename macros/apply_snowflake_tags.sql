60565e7c612b664140f9167ddf7962efe285b19c
bp-jeremy-nguyen-cp <bp_jeremy_nguyen@colpal.com>
2026-05-26 11:12:30 -0400

    update syntax set var

--- macros/apply_snowflake_tags.sql
+++ macros/apply_snowflake_tags.sql
@@ -330,7 +330,7 @@
    ============================================================ #}
 {% macro call_cross_domain_views_read_proc() %}
     {% if execute %}
-        {% deploy_db = env_var('DEPLOY_DATABASE', target.database | default('')) | upper %}
+        {% set deploy_db = env_var('DEPLOY_DATABASE', target.database | default('')) | upper %}
 
         {{ log("Calling GRANT_CROSS_DOMAIN_READ_ACCESS procedure to apply crpss domain read grants", info=true) }}
         {% do run_query("CALL OPS_CUR.UTIL_COMMON.GRANT_CROSS_DOMAIN_READ_ACCESS('" ~ deploy_db ~ "')") %}
