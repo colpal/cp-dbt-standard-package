95fd71107b09b6bf104db64aa276c3758c29535c
Othniel Alexander <95395932+othniel0706@users.noreply.github.com>
2026-06-05 14:10:57 +0530

    fix: cast ARRAY/OBJECT to VARCHAR but keep VARIANT as-is - Iceberg supports VARIANT but not ARRAY/OBJECT storage types

--- macros/iceberg_overrides.sql
+++ macros/iceberg_overrides.sql
@@ -207,7 +207,8 @@ PURPOSE:    Globally intercepts dbt's native Snowflake materialization macros to
             
             {%- set is_unspecified_number = ('NUMBER' in col_type or 'DECIMAL' in col_type or 'NUMERIC' in col_type) and ('(' not in col_type or '38,0' in stripped_type) -%}
             
-            {%- if 'TIMESTAMP' in col_type or 'TIME' in col_type or 'VARCHAR' in col_type or 'STRING' in col_type or is_unspecified_number -%}
+            {%- set is_array_or_object = ('ARRAY' in col_type or 'OBJECT' in col_type) and 'VARIANT' not in col_type -%}
+            {%- if 'TIMESTAMP' in col_type or 'TIME' in col_type or 'VARCHAR' in col_type or 'STRING' in col_type or is_unspecified_number or is_array_or_object -%}
                 {%- do needs_casting.append(col_name) -%}
             {%- endif -%}
             {%- do final_columns.append({'name': col_name, 'type': col_type}) -%}
@@ -243,6 +244,8 @@ PURPOSE:    Globally intercepts dbt's native Snowflake materialization macros to
                 CAST("{{ col_name }}" AS TIMESTAMP_NTZ(6)) AS "{{ col_name }}"
             {%- elif 'TIME' in col_type -%}
                 CAST("{{ col_name }}" AS TIME(6)) AS "{{ col_name }}"
+            {%- elif is_array_or_object -%}
+                CAST(TO_JSON("{{ col_name }}") AS VARCHAR(16777216)) AS "{{ col_name }}"
             {%- elif 'VARCHAR' in col_type or 'STRING' in col_type -%}
                 CAST("{{ col_name }}" AS VARCHAR(16777216)) AS "{{ col_name }}"
             {%- elif is_unspecified_number -%}
