0b7065ede071135ae2c9ee2dd603d81833747dd8
TanviBhagwat23 <81402810+TanviBhagwat23@users.noreply.github.com>
2026-08-13 17:42:24 +0530

    Update apply_snowflake_tags.sql

--- macros/apply_snowflake_tags.sql
+++ macros/apply_snowflake_tags.sql
@@ -2,7 +2,7 @@
   Snowflake Tagging Package (`dbt_snowflake_tagging`)
   ----------------------------------------------------
     A set of macros to apply centrally created Snowflake tags to dbt models and columns
-    based on configurations in schema YAML files.
+    based on configurations in schema YAML files
 
   Version: 1.0.0
 
